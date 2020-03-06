398afe32da794662a87a2ceb40bf16488bdb4520
Joseph Stout <joseph.benedikt.stout@gmail.com>
2020-03-06 14:29:09 +0100

    filter urls

--- wohnungsmarkt_request.py
+++ wohnungsmarkt_request.py
@@ -5,9 +5,9 @@ import time
 wg = WgGesucht(1, "Augsburg")
 p_cnt = wg.get_page_counter()
 for i in range(p_cnt):
-    urls = wg.get_urls(i)
+    urls_a = wg.get_urls(i)
+    urls = [x for x in urls_a if wg.get_id_of_url(x) not in wg.inserat_ids]
     for url in urls:
         parsed_wg = wg.parse_wgs(url)
-        if parsed_wg["inserat_id"] not in wg.inserat_ids:
-            wg.insert_into_inserate(parsed_wg)
-            time.sleep(random.choice([4, 5, 6, 7, 8]))
+        wg.insert_into_inserate(parsed_wg)
+        time.sleep(random.choice([4, 5, 6, 7, 8]))
