71fcb0614498762cf9bf66e58e944f59a028c1bf
Joseph Stout <joseph.benedikt.stout@gmail.com>
2020-05-05 11:11:24 +0200

    added senkelbach

--- wg_gesucht_parser.py
+++ wg_gesucht_parser.py
@@ -88,6 +88,7 @@ viertel_map = {
     "links der wertach": "Oberhausen_Augsburg",
     "links der wertach-süd": "Oberhausen_Augsburg",
     "man viertel": "Oberhausen_Augsburg",
+    "senkelbach": "Oberhausen_Augsburg",
     "zentrum": "Innenstadt_Augsburg",
     "textilviertel": "Spickel-Herrenbach_Augsburg",
     "textielviertel": "Spickel-Herrenbach_Augsburg",
